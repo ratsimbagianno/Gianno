af0e68f0f452179a4926f8d7ad38a5f83386ecd9
ratsimbagianno <167101612+ratsimbagianno@users.noreply.github.com>
2024-04-15 17:15:17 +0300

    Add files via upload

---
+++ gina exo2.py
@@ -0,0 +1,26 @@
+
+ 
+   def fonction logique(A,B,C):
+     
+     #Exemple de fonction: F(A,B,C)= A B + not B C + A not C
+      
+def forme canonique(A,B,C)
+fnc: F(A,B,C)= not A not B C + A not B C + A B not C + A B C
+
+ # Affichage de la table de verite
+ print("A,B,C | F(A,B,C)")
+ print("----------------")
+ 
+ def generer_table_de_verite:
+     variable: [0,1] # 0 pour faux, 1 pour vrai
+     table = A B | 00 | 01 | 11 | 10
+                   C  
+                    ------------------------------
+                   0    |  0   |  0  | 1  | 1  
+                   1    |  1   |  0  |  1 | 1
+                   
+    for ligne in table:
+        A,B,C resultat = colonne adjacente
+        if resultat:
+ def fonction_simplifier(F("A,B,C")= A + not B C )
+     
